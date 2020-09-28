237e4f538edaa6f6a2155df7b0f5a32a2d811f99
SGokilapriya <36201974+SGokilapriya@users.noreply.github.com>
2020-09-28 23:37:10 +0530

    Adding a test script

---
+++ Assets/PlayTest/NewTestScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [SetUp]
+        public void setup()
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+
+        [UnityTest]
+        public IEnumerator SceneIsLoaded()
+        {
+            Assert.AreEqual(SceneManager.GetActiveScene().name, "SampleScene");
+            yield return null;
+        }
+      
+        
+    }
+}
