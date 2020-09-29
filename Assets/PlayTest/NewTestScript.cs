6c1a941f36525bf86a3072dd3de88b64b1b35bc0
SGokilapriya <36201974+SGokilapriya@users.noreply.github.com>
2020-09-29 17:52:18 +0530

    created a scene in test script

--- Assets/PlayTest/NewTestScript.cs
+++ Assets/PlayTest/NewTestScript.cs
@@ -12,19 +12,84 @@ namespace Tests
 {
     public class NewTestScript
     {
+        GameObject plane;
+        GameObject[] wall;
+        GameObject dice;
+
         [SetUp]
         public void setup()
         {
-            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            wall = new GameObject[4];
+            for (int i = 0; i < 4; i++)
+            {
+                wall[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                wall[i].name = "wall" + i;
+            }
+            dice = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            CreateEnvironment();
+        }
+
+        void CreateEnvironment()
+        {
+            SetPlane(plane);
+            for (int i = 0; i < 4; i++)
+            {
+                SetWall(wall[i], i);
+            }
+            SetDice(dice);
+        }
+        void SetPlane(GameObject obj)
+        {
+            obj.transform.localPosition = Values.plane_pos;
+            //obj.transform.localRotation = Values.plane_rot;
+            obj.transform.localScale = Values.plane_scale;
+        }
+
+        void SetWall(GameObject obj, int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    obj.transform.localPosition = Values.cube1_pos;
+                   // obj.transform.localRotation = Values.cube1_rot;
+                    obj.transform.localScale = Values.cube_scale;
+                    break;
+                case 1:
+                    obj.transform.localPosition = Values.cube2_pos;
+                    //obj.transform.localRotation = Values.cube2_rot;
+                    obj.transform.localScale = Values.cube_scale;
+                    break;
+                case 2:
+                    obj.transform.localPosition = Values.cube3_pos;
+                    obj.transform.localScale = Values.wall3and4;
+                    break;
+                case 3:
+                    obj.transform.localPosition = Values.cube4_pos;
+                    obj.transform.localScale = Values.wall3and4;
+                    break;
+            }
+            obj.GetComponent<Renderer>().material.color = Color.blue;
+            
+        }
+
+        void SetDice(GameObject obj)
+        {
+            obj.name = "dice";
+            obj.transform.position = new Vector3(0f, 3.59f, 0.81f);
+            obj.transform.Rotate(-45f, -50f, 40f);
+            obj.GetComponent<Renderer>().material.color = Color.yellow;
+            //obj.GetComponent<Rigidbody>();
         }
 
         [UnityTest]
-        public IEnumerator SceneIsLoaded()
+        public IEnumerator MoveFront()
         {
-            Assert.AreEqual(SceneManager.GetActiveScene().name, "SampleScene");
-            yield return null;
-        }
-      
         
+            yield return null;
+            yield return null;
+            
+        }
     }
 }
